46288dbf25df99f409b0da21e2a3c40592f3d5b4
Erick <paiva.erick2001@gmail.com>
2026-08-05 12:53:32 -0300

    propriedade componente e integracao com css 1

--- exercicios/pages/basicos/usandoTitulo.jsx
+++ exercicios/pages/basicos/usandoTitulo.jsx
@@ -1,9 +1,18 @@
 import Titulo from "../../components/Titulo"
 
+
 export default function usandoTitulo() {
     return (
         <div>
-            <Titulo />
+            <Titulo 
+                principal="Página de cadastro" 
+                secundario="Incluir, alterar e excluir coisas!"
+            />
+            <Titulo 
+                principal="Página de Login" 
+                secundario="Informe o seu email e senha"
+                pequeno
+            />
         </div>
     )
 }
