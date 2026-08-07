eae0708198b7fd0ead5e7d50c29b0508948c885f
Erick <paiva.erick2001@gmail.com>
2026-08-07 09:43:46 -0300

    props children

---
+++ exercicios/pages/basicos/componenteComFilho.jsx
@@ -0,0 +1,18 @@
+import Item from "../../components/Item"
+import Lista from "../../components/Lista"
+
+export default function componenteComFilho() {
+    return (
+        <div>
+            <Lista>
+                <Item conteudo="Item #01" />
+                <Item conteudo="Item #02" />
+                <Item conteudo="Item #03" />
+                <Item conteudo="Item #04" />
+                <Item conteudo="Item #05" />
+                <Item conteudo="Item #06" />
+                <Item conteudo="Item #07" />
+            </Lista>
+        </div>
+    )    
+}
