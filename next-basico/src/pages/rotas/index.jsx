2b0d1fce2fbd2795a5b656cc62eaf3d58407dd85
Erick <paiva.erick2001@gmail.com>
2026-08-28 10:36:26 -0300

    navegando entre paginas #01

--- next-basico/src/pages/rotas/index.jsx
+++ next-basico/src/pages/rotas/index.jsx
@@ -1,7 +1,17 @@
+import Link from 'next/link'
+
 export default function rotas() {
     return (
         <div>
             <h1>Rotas Index</h1>
+            <ul>
+                <Link href="/rotas/params?id=7&nome=Erick">
+                    <li>Params</li>
+                </Link>
+                <Link href="/rotas/123/buscar">
+                    <li>Buscar</li>
+                </Link>
+            </ul>
         </div>
     )
 }
